1f75f21861b3ee0506bce28d61d4647bac535e54
Jasmine Emerson <Jazafras@users.noreply.github.com>
2016-02-26 14:57:21 -0800

    Update rot90.cpp

--- rot90.cpp
+++ rot90.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 int main() {
   std::vector<std::string> lines;
-
+  std::size_t max_line_size = 0;
   std::string line;
   while (std::getline(std::cin, line)) {
     lines.push_back(line);
+    if( max_line_size < line.size()){
+        max_line_size = line.size(); //store length of longest string
+    }
   }
-
-  // Insert code here
-  for(auto s : lines) {		//for every line stored in vector
-	cout << s;		//print each line
-	for(auto c : s) {
-		
-	}
-}
-
+  std::reverse(lines.begin(),lines.end());
+  //print vertically
+  for( std::size_t i=0; i<max_line_size; i++){
+    for( const std::string& line : lines){
+        std::cout << ( line.size() > i ? line[i] : ' ');
+    }
+    std::cout << '\n';
+  }
   return 0;
 }
